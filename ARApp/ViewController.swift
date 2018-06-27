8185ce5aea3963c1ecbdc0d601b33827d98a13b4
TACITE David <david.tacite@altran.com>
2018-06-27 11:38:23 +0200

    Initial Commit

---
+++ ARApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ARApp
+//
+//  Created by David Tacite on 27/06/2018.
+//  Copyright © 2018 David Tacite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
